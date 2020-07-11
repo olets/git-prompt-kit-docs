3a15bdca18e4a80d74238ca81adec7e1e86f0891
Henry Bley-Vroman <olets@olets.dev>
2020-07-11 19:17:02 -0400

    feat(site): remove unused features

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,10 @@
 /**
  * A Gulp setup for static site building.
- * Handles images, markup (HTML), scripts, and styles ((Post)CSS, LESS, Sass).
+ * Handles markup (HTML), scripts, and LESS styles.
  * File structure is completely up to you.
  * Supports bundling of select stylesheets.
+ *
+ * Copyright (c) Henry Bley-Vroman, 2020
  *
  * Basic setup: configure
  * - PostCSS
@@ -23,7 +25,7 @@
 import browserSync from 'browser-sync'
 import {
   dest, // used by all tasks
-  lastRun, // used by images, scripts, styles, and views tasks
+  lastRun, // used by scripts, styles, and views tasks
   parallel, // used by build task
   series, // used by watches, build, and dev tasks
   src, // used by all tasks
@@ -36,15 +38,12 @@ import webpack from 'webpack' // used by scripts task
 import webpackDevMiddleware from 'webpack-dev-middleware' // used by serve task
 import webpackHotMiddleware from 'webpack-hot-middleware' // used by serve task
 
-const concat = require('gulp-concat') // used by stylesBundle task
 const debug = require('gulp-debug') // used by all tasks
-const filter = require('gulp-filter') // used by styles task and stylesBundle task
-const less = require('gulp-less') // used by styles task and stylesBundle task
+const filter = require('gulp-filter') // used by styles task
+const less = require('gulp-less') // used by styles task
 const plumber = require('gulp-plumber') // used by all tasks
-const postcss = require('gulp-postcss') // used by styles task and stylesBundle task
+const postcss = require('gulp-postcss') // used by styles task
 const rename = require('gulp-rename') // used by views task
-const sass = require('gulp-sass') // used by styles task and stylesBundle task
-const svgstore = require('gulp-svgstore') // used by styles task and stylesBundle task
 const twig = require('gulp-twig') // used by views task
 
 import { config as webpackConfig } from './webpack'
@@ -65,20 +64,13 @@ const debugOptions = title => {
   }
 }
 const mode = process.env.NODE_ENV || 'development'
-const server = browserSync //.create()
+const server = browserSync.create()
 
 /**
  * PostCSS
  */
 
 const postCssPlugins = [
-  /**
-   * Add PostCSS plugins here, e.g.
-   *
-   * require('tailwindcss')('./tailwind.config.js')
-   *
-   */
-  // require('postcss-easy-import'),
   require('postcss-csso')({comments: false}),
   require('postcss-preset-env')()
 ]
@@ -90,28 +82,6 @@ const postCssPlugins = [
 const destinationDir = 'dist'
 const sourceDir = 'src'
 const taskConfig = {
-  /**
-   * Each task's config has a srcGlobs parameter and optional srcOptions parameter
-   * which are passed to the task's `gulp.src`
-   * (see https://gulpjs.com/docs/en/api/src)
-   *
-   * and a destDirectory parameter and optional destOptions parameter
-   * which are passed to the task's `gulp.dest`
-   * (see https://gulpjs.com/docs/en/api/dest)
-   *
-   */
-  images: {
-    destDirectory: destinationDir,
-    destOptions: null,
-    srcGlobs: [
-      `${sourceDir}/**/*.{gif,jpg,png,svg}`,
-      `!${sourceDir}/**/_*/**/*.{gif,jpg,png,svg}`, /* ignore files in underscore-prefixed directories */
-      `!${sourceDir}/**/_*.{gif,jpg,png,svg}` /* ignore underscore-prefixed files */
-    ],
-    srcOptions: {
-      since: lastRun(images)
-    },
-  },
   scripts: {
     destDirectory: destinationDir,
     destOptions: null,
@@ -125,43 +95,19 @@ const taskConfig = {
       sourcemaps: mode != 'production'
     },
   },
-  stylesBundle: {
-    destDirectory: destinationDir,
-    destOptions: null,
-    srcGlobs: [
-      `${sourceDir}/**/*.bundle.{css,less,scss}`,
-      // `!${sourceDir}/**/_*/**/*.bundle.{css,less,scss}`, /* ignore files in underscore-prefixed directories */
-      `!${sourceDir}/**/_*.bundle.{css,less,scss}` /* ignore underscore-prefixed files */
-    ],
-    srcOptions: {
-      sourcemaps: mode != 'production'
-    },
-    targetFile: 'bundle.css',
-  },
   styles: {
     destDirectory: destinationDir,
     destOptions: null,
     srcGlobs: [
-      `${sourceDir}/**/*.{css,less,scss}`,
-      `!${sourceDir}/**/*.bundle.{css,less,scss}`, /* ignore files that get bundled */
-      `!${sourceDir}/**/_*/**/*.{css,less,scss}`, /* ignore files in underscore-prefixed directories */
-      `!${sourceDir}/**/_*.{css,less,scss}` /* ignore underscore-prefixed files */
+      `${sourceDir}/**/*.less`,
+      `!${sourceDir}/**/*.bundle.less`, /* ignore files that get bundled */
+      `!${sourceDir}/**/_*/**/*.less`, /* ignore files in underscore-prefixed directories */
+      `!${sourceDir}/**/_*.less` /* ignore underscore-prefixed files */
     ],
     srcOptions: {
       since: lastRun(styles),
       sourcemaps: mode != 'production'
     },
-  },
-  svgSprite: {
-    destDirectory: destinationDir,
-    destOptions: null,
-    srcGlobs: [
-      `${sourceDir}/**/*.svg`,
-      `!${sourceDir}/**/_*/**/*.svg`, /* ignore files in underscore-prefixed directories */
-      `!${sourceDir}/**/_*.svg` /* ignore underscore-prefixed files */
-    ],
-    srcOptions: null,
-    svgstore: null,
   },
   views: {
     destDirectory: destinationDir,
@@ -180,16 +126,6 @@ const taskConfig = {
 /**
  * Tasks
  */
-
-function images(done) {
-  src(taskConfig.images.srcGlobs, taskConfig.images.srcOptions || null )
-    .pipe(plumber())
-    .pipe(debug(debugOptions('Images:')))
-    .pipe(dest(taskConfig.images.destDirectory, taskConfig.images.destOptions || null ))
-    .pipe(server.stream())
-
-  done()
-}
 
 function reload(done) {
   server.reload()
@@ -227,57 +163,12 @@ function serve(done) {
 }
 
 function styles(done) {
-  const lessFilter = filter('**/*.less', {restore: true})
-  const sassFilter = filter('**/*.scss', {restore: true})
-
   src(taskConfig.styles.srcGlobs, taskConfig.styles.srcOptions || null )
     .pipe(plumber())
-    .pipe(debug(debugOptions('All non-bundled stylesheets:')))
-    // .pipe(dirFilter)
-    .pipe(lessFilter)
     .pipe(less())
-    .pipe(debug(debugOptions('LESS non-bundled stylesheets:')))
-    .pipe(lessFilter.restore)
-    .pipe(sassFilter)
-    .pipe(debug(debugOptions('Sass non-bundled stylesheets:')))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sassFilter.restore)
     .pipe(postcss(postCssPlugins))
     .pipe(dest(taskConfig.styles.destDirectory, taskConfig.styles.destOptions || null ))
     .pipe(server.stream())
-
-  done()
-}
-
-function stylesBundle(done) {
-  const bundleLessFilter = filter('**/*.bundle.less', {restore: true})
-  const bundleSassFilter = filter('**/*.bundle.scss', {restore: true})
-
-  src(taskConfig.stylesBundle.srcGlobs, taskConfig.stylesBundle.srcOptions || null )
-    .pipe(plumber())
-    .pipe(debug(debugOptions('All bundled stylesheets:')))
-    .pipe(bundleLessFilter)
-    .pipe(less())
-    .pipe(debug(debugOptions('LESS bundled stylesheets:')))
-    .pipe(bundleLessFilter.restore)
-    .pipe(bundleSassFilter)
-    .pipe(debug(debugOptions('Sass bundled stylesheets:')))
-    .pipe(sass())
-    .pipe(bundleSassFilter.restore)
-    .pipe(postcss(postCssPlugins))
-    .pipe(concat(taskConfig.stylesBundle.targetFile))
-    .pipe(dest(taskConfig.stylesBundle.destDirectory, taskConfig.stylesBundle.destOptions || null ))
-    .pipe(server.stream())
-
-  done()
-}
-
-function svgSprite(done) {
-  src(taskConfig.svgSprite.srcGlobs, taskConfig.svgSprite.srcOptions || null)
-    .pipe(plumber())
-    .pipe(debug(debugOptions('svgSprite:')))
-    .pipe(svgstore(taskConfig.svgSprite.svgstore || null))
-    .pipe(dest(taskConfig.svgSprite.destDirectory, taskConfig.svgSprite.destOptions || null))
 
   done()
 }
@@ -286,7 +177,7 @@ function views(done) {
   const twigFilter = filter('**/*.{html.twig,twig}', {restore: true})
   const htmlTwigFilter = filter('**/*.html.twig', {restore: true})
 
-  src(taskConfig.views.srcGlobs)
+  src(taskConfig.views.srcGlobs, taskConfig.styles.srcOptions || null )
     .pipe(plumber())
     .pipe(debug(debugOptions('Views:')))
     .pipe(twigFilter)
@@ -307,13 +198,11 @@ function views(done) {
 }
 
 function watches() {
-  watch(taskConfig.images.srcGlobs, images)
   watch(taskConfig.scripts.srcGlobs, series(
     scripts,
     reload
   ))
   watch(taskConfig.styles.srcGlobs, styles)
-  watch(taskConfig.stylesBundle.srcGlobs, stylesBundle)
   watch(taskConfig.views.srcGlobs, series(
     views,
     reload
@@ -327,7 +216,7 @@ function watches() {
 const clean = () => del(destinationDir)
 const build = series(
   clean,
-  parallel(images, scripts, styles, stylesBundle, svgSprite),
+  parallel(scripts, styles),
   views
 )
 const dev = series(
@@ -353,13 +242,10 @@ export {
   build,
   clean,
   dev,
-  images,
   reload,
   scripts,
   serve,
   styles,
-  stylesBundle,
-  svgSprite,
   views,
   watches,
 }
