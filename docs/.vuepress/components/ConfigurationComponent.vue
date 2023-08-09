8da575f3505dfddaddb8b1efa53695e782788060
Henry Bley-Vroman <olets@olets.dev>
2023-08-09 12:49:59 -1000

    feat(ConfigurationComponent): validate inputs

--- docs/.vuepress/components/ConfigurationComponent.vue
+++ docs/.vuepress/components/ConfigurationComponent.vue
@@ -9,14 +9,32 @@ export default {
   },
   methods: {
     ansiOrHexColor,
+    getPattern(type) {
+      const patt = type === "array" ? "\\(.*\\)" : null;
+      return patt;
+    },
     hexColor,
-    set(key, value, isColor = false) {
-      if (isColor) {
-        this.store.options[key].custom = ansiOrHexColor(value);
+    set(key, target, isColor = false) {
+      const valid = !target.validity.patternMismatch;
+      const pattern = target.getAttribute("pattern");
+
+      target.setCustomValidity("");
+      target.reportValidity();
+
+      if (!valid) {
+        target.setCustomValidity(
+          `Invalid: must match the pattern /${pattern}/`
+        );
+        target.reportValidity();
         return;
       }
 
-      this.store.options[key].custom = value;
+      if (isColor) {
+        this.store.options[key].custom = ansiOrHexColor(target.value);
+        return;
+      }
+
+      this.store.options[key].custom = target.value;
     },
     reset() {
       this.store.$reset;
@@ -26,22 +44,30 @@ export default {
 </script>
 
 <template>
-  <p v-if="store.customizations" style="color: red">Modified from default</p>
-  <p v-if="!store.customizations">Not modified from default</p>
+  <p
+    v-if="store.customizations"
+    style="color: red"
+  >
+    Modified from default
+  </p>
+  <p v-if="!store.customizations">
+    Not modified from default
+  </p>
 
   <p>
     Each color configuration variable can be either an
-    <a href="https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit"
-      >8-bit ANSI color escape code</a
-    >
+    <a href="https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit">8-bit ANSI color escape code</a>
     or a
-    <a href="https://en.wikipedia.org/wiki/Web_colors#Hex_triplet"
-      >hexadecimal</a
-    >
+    <a href="https://en.wikipedia.org/wiki/Web_colors#Hex_triplet">hexadecimal</a>
     triplet.
   </p>
 
-  <button style="margin-top: 20px" @click="reset">Reset</button>
+  <button
+    style="margin-top: 20px"
+    @click="reset"
+  >
+    Reset
+  </button>
 
   <form style="display: grid; gap: 8px">
     <table>
@@ -54,34 +80,52 @@ export default {
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(value, key) in store.options" :key="key">
+        <tr
+          v-for="(option, key) in store.options"
+          :key="key"
+          :class="
+            option.custom && option.custom != option.default
+              ? 'customized'
+              : null
+          "
+        >
           <td>
             <label :for="`field-${key}`">
               <code>{{ key }}</code>
             </label>
           </td>
 
-          <td>{{ value.default }}</td>
-          <td>{{ value.type }}</td>
+          <td>{{ option.default }}</td>
+          <td>{{ option.type }}</td>
 
-          <td style="display: flex; gap: 1rem">
-            <input
-              :max="value.type.includes('boolean') ? 1 : null"
-              min="0"
-              :name="`field-${key}`"
-              :placeholder="value.default"
-              style="text-align: right; flex-grow: 1"
-              :type="value.type.includes('integer') ? 'number' : 'text'"
-              :value="value.custom || value.default"
-              @input="({ target }) => set(key, target.value)"
-            />
+          <td>
+            <div style="display: flex; gap: 1rem">
+              <input
+                :max="option.type.includes('boolean') ? 1 : null"
+                min="0"
+                :name="`field-${key}`"
+                :placeholder="option.default"
+                style="text-align: right; flex-grow: 1"
+                :type="option.type.includes('integer') ? 'number' : 'text'"
+                :value="option.custom || option.default"
+                :pattern="getPattern(option.type)"
+                @change="({ target }) => set(key, target)"
+              >
 
-            <input
-              v-if="value.type.includes('hex')"
-              type="color"
-              :value="hexColor(value.custom || value.default)"
-              @input="({ target }) => set(key, target.value, true)"
-            />
+              <input
+                v-if="option.type.includes('hex')"
+                type="color"
+                :value="hexColor(option.custom || option.default)"
+                @input="({ target }) => set(key, target, true)"
+              >
+            </div>
+
+            <div
+              v-if="getPattern(option.type)"
+              class="validity"
+            >
+              Must match /{{ getPattern(option.type) }}/
+            </div>
           </td>
         </tr>
       </tbody>
