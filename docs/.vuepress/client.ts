ccc54e56296d2ab9be3e7848ff233aa341a0a4ef
Henry Bley-Vroman <olets@olets.dev>
2023-08-09 16:53:50 -1000

    chore(components): remove unused

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -3,12 +3,7 @@ import { createPinia } from "pinia";
 import "a11y-dialog";
 const pinia = createPinia();
 import OptionsConfigurationComponent from "./components/OptionsConfigurationComponent.vue";
-import ContextComponent from "./components/ContextComponent.vue";
 import PromptComponent from "./components/PromptComponent.vue";
-import ColorComponent from "./components/ColorComponent.vue";
-import ColorOptionsComponent from "./components/ColorOptionsComponent.vue";
-import GitOptionsComponent from "./components/GitOptionsComponent.vue";
-import UserOptionsComponent from "./components/UserOptionsComponent.vue";
 import ResetOptionsComponent from "./components/ResetOptionsComponent.vue";
 import ConfigComponent from "./components/ConfigComponent.vue";
 
@@ -19,12 +14,7 @@ export default defineClientConfig({
       "OptionsConfigurationComponent",
       OptionsConfigurationComponent
     );
-    app.component("ContextComponent", ContextComponent);
     app.component("PromptComponent", PromptComponent);
-    app.component("ColorOptionsComponent", ColorOptionsComponent);
-    app.component("ColorComponent", ColorComponent);
-    app.component("GitOptionsComponent", GitOptionsComponent);
-    app.component("UserOptionsComponent", UserOptionsComponent);
     app.component("ResetOptionsComponent", ResetOptionsComponent);
     app.component("ConfigComponent", ConfigComponent);
   },
