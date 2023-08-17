35df77ff5ec76e0c1db9dd2cd36b7c52c7a6b2e1
Henry Bley-Vroman <olets@olets.dev>
2023-08-17 11:17:39 -1000

    docs(commitlint): add comments to config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,18 @@
+/**
+ * <type>(<scope>): <subject> // this whole thing is <header>
+ *
+ * <body>
+ *
+ * <footer>
+ */
+
 // eslint-disable-next-line no-undef
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-max-line-length": [1, "always", 100],
-    "subject-full-stop": [1, "never", "."],
-    "subject-case": [0, "always"],
-    "header-max-length": [1, "always", 72],
+    "body-max-line-length": [1, "always", 100], // warn if body length exceeds 100 characters (default is error)
+    "header-max-length": [1, "always", 72], // warn if message length exceeds 72 characters (default is error over 100)
+    "subject-case": [0, "always"], // support any case in message (default is error on lowercase)
+    "subject-full-stop": [1, "never", "."], // support ending a message with `...` (default is error when ending in `.`)
   },
 };
