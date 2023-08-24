6a4d804c45644e373b0b8d46722bb05acdf0dd8a
Henry Bley-Vroman <olets@olets.dev>
2023-08-23 15:43:35 -1000

    fix(Remote): no upstream markup if no upstream

--- docs/.vuepress/components/GitRemoteComponent.vue
+++ docs/.vuepress/components/GitRemoteComponent.vue
@@ -13,6 +13,33 @@ export default {
     };
     return { store };
   },
+  computed: {
+    remoteName() {
+      return `${
+        !valueOf(this.store.context.data.gitRefDefaultRemote) ? "upstream" : ""
+      }${
+        !valueOf(this.store.context.data.gitRefDefaultRemote) &&
+        !valueOf(this.store.context.data.gitRefSameNameRemoteBranch)
+          ? "/"
+          : ""
+      }${
+        !valueOf(this.store.context.data.gitRefSameNameRemoteBranch)
+          ? "trunk"
+          : ""
+      }`;
+    },
+    remoteSymbol() {
+      return (
+        valueOf(this.store.options.data.GIT_PROMPT_KIT_SYMBOL_REMOTE) &&
+        valueOf(this.store.context.data.gitPushRefPush) &&
+        (valueOf(this.store.context.data.gitRefAhead) ||
+          valueOf(this.store.context.data.gitRefBehind) ||
+          !valueOf(
+            this.store.options.data.GIT_PROMPT_KIT_HIDE_INACTIVE_AHEAD_BEHIND
+          ))
+      );
+    },
+  },
   methods: {
     useVerboseDefaults() {
       return valueOf(
@@ -37,54 +64,34 @@ export default {
   >
     {{ valueOf(store.options.data.GIT_PROMPT_KIT_SYMBOL_LOCAL) }}
   </span>
+
   <!-- upstream -->
   <span
-    v-if="valueOf(store.context.data.gitRefRemote)"
+    v-if="
+      valueOf(store.context.data.gitRefRemote) && (remoteSymbol || remoteName)
+    "
     id="upstream"
     style="display: flex; gap: var(--prompt-gap)"
   >
     <!-- remote symbol -->
-    <span
-      v-if="
-        valueOf(store.options.data.GIT_PROMPT_KIT_SYMBOL_REMOTE) &&
-          valueOf(store.context.data.gitPushRefPush)
+    <PromptSegmentComponent
+      v-if="remoteSymbol"
+      :key="useVerboseDefaults()"
+      :color-option="
+        valueOf(store.context.data.gitRefAhead) ||
+          valueOf(store.context.data.gitRefBehind)
+          ? 'GIT_PROMPT_KIT_COLOR_REMOTE'
+          : 'GIT_PROMPT_KIT_COLOR_INACTIVE'
       "
-    >
-      <PromptSegmentComponent
-        v-if="
-          valueOf(store.context.data.gitRefAhead) ||
-            valueOf(store.context.data.gitRefBehind) ||
-            !valueOf(store.options.data.GIT_PROMPT_KIT_HIDE_INACTIVE_AHEAD_BEHIND)
-        "
-        :key="useVerboseDefaults()"
-        :color-option="
-          valueOf(store.context.data.gitRefAhead) ||
-            valueOf(store.context.data.gitRefBehind)
-            ? 'GIT_PROMPT_KIT_COLOR_REMOTE'
-            : 'GIT_PROMPT_KIT_COLOR_INACTIVE'
-        "
-        :text="valueOf(store.options.data.GIT_PROMPT_KIT_SYMBOL_REMOTE)"
-      />
-    </span>
+      :text="remoteSymbol"
+    />
 
     <!-- remote and remote branch -->
     <PromptSegmentComponent
-      v-if="
-        !valueOf(store.context.data.gitRefDefaultRemote) ||
-          !valueOf(store.context.data.gitRefSameNameRemoteBranch)
-      "
+      v-if="remoteName"
       :key="useVerboseDefaults()"
       color-option="GIT_PROMPT_KIT_COLOR_REMOTE"
-      :text="`${
-        !valueOf(store.context.data.gitRefDefaultRemote) ? 'upstream' : ''
-      }${
-        !valueOf(store.context.data.gitRefDefaultRemote) &&
-        !valueOf(store.context.data.gitRefSameNameRemoteBranch)
-          ? '/'
-          : ''
-      }${
-        !valueOf(store.context.data.gitRefSameNameRemoteBranch) ? 'trunk' : ''
-      }`"
+      :text="remoteName"
     />
   </span>
 </template>
